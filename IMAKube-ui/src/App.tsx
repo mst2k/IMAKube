886040b8ecb5e7eeffc040487c3b544b2cfc91bd
22marcel11 <marcel.strohm@web.de>
2024-08-29 11:15:19 +0000

    init frontend

---
+++ IMAKube-ui/src/App.tsx
@@ -0,0 +1,30 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [number, setNumber] = useState<number>(0);
+  const [isEven, setIsEven] = useState<boolean | undefined>();
+
+  async function getData(num: number) {
+    const url = `http://localhost:8080/iseven?number=${num}`;
+    const response = await fetch(url);
+    const data = await response.json();
+    setIsEven(data.is_even);
+  }
+
+  const handleChangeNumber = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setNumber(parseInt(e.target.value));
+  };
+
+  return (
+    <>
+      <input type='number' value={number} onChange={handleChangeNumber} /> 
+      <button onClick={() => getData(number)}>Check if the number is even</button>
+      {isEven !== undefined && (
+        <p>The number is {isEven ? 'even' : 'odd'}</p>
+      )}
+    </>
+  );
+}
+
+export default App;
